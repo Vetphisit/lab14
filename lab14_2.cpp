a6b1f65a5673f8ab4c0c91ca48fdb70ac0373ebf
Vetphisit <vetpisit07@gmail.com>
2025-01-26 23:05:24 +0700

    done

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -49,7 +49,7 @@ void findLocalMax(const double A[N][N], bool B[N][N]){
     }
     for(int i=1;i<N-1;i++){
         for(int j=1;j<N-1;j++){
-            if(A[i][j]>=A[i-1][j]&&
+            if(A[i][j]>=A[i-1][j] && 
                 A[i][j]>=A[i+1][j]&&
                 A[i][j]>=A[i][j-1]&&
                 A[i][j]>=A[i][j+1]){
